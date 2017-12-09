70a4ce8d1a2a14780134d75a15bdd82bcb1e649b
Christopher Szatmary <christopherszatmary@gmail.com>
2017-12-09 15:58:21 -0500

    Added isWithinRange function

--- src/components/functions.js
+++ src/components/functions.js
@@ -1,11 +1,17 @@
 // @flow
 
-export default function randomInt(min: number, max: number): number {
+function randomInt(min: number, max: number): number {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min)) + min;
 }
 
+function isWithinRange(value: number, min: number, max: number): boolean {
+    return value >= min && value <= max;
+}
+
 Array.prototype.equals = function(array: Array<any>) {
     return this.length === array.length && this.every((el, i) => el === array[i]);
 };
+
+export { randomInt, isWithinRange };
