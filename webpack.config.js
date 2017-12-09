39dc4ce207a5fca4ea171827e2ac7adfef0c1c01
Christopher Szatmary <christopherszatmary@gmail.com>
2017-12-08 20:41:33 -0500

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+const webpack = require('webpack');
+
+const resolve = path.resolve,
+    join = path.join;
+
+const rootDir = resolve(__dirname);
+const srcDir = join(rootDir, 'src');
+const buildDir = join(rootDir, 'build');
+
+module.exports = {
+    entry: join(srcDir, 'index.js'),
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                include: srcDir,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['env']
+                    }
+                }
+            }
+        ]
+    },
+    output: {
+        path: buildDir,
+        filename: 'index.bundle.js'
+    }
+};
