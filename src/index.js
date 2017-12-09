80b20e5e1908537875818190490832fe2d337da5
Christopher Szatmary <christopherszatmary@gmail.com>
2017-12-09 00:10:33 -0500

    Set up a basic sketch

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-console.log("Hello Webpack + Babel!");
+import p5 from 'p5';
+import createSketch from './components/createSketch';
+import Color from './components/Color';
+
+createSketch(window, 500, 500, new Color(0, 100, 200));
+
+window.draw = function() {
+    const c = new Color(100, 0, 0);
+    fill(c.getP5Color());
+    ellipse(200, 200, 50, 50);
+};
+
