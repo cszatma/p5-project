8b090e2f48ebf1e0e4c05d8a3d789218444060ca
cszatma <christopherszatmary@gmail.com>
2017-12-09 19:09:44 -0500

    Added Point and Size classes

---
+++ src/components/Size.js
@@ -0,0 +1,13 @@
+// @flow
+
+class Size {
+    width: number;
+    height: number;
+
+    constructor(width: number, height: number) {
+        this.width = width;
+        this.height = height;
+    }
+}
+
+export default Size;
