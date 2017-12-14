9d6b05db7072745c8e7dc92eed619dce2fb4f656
Christopher Szatmary <christopherszatmary@gmail.com>
2017-12-13 22:16:08 -0500

    Added intervalSince() method to Date, created a timer in index.js

--- src/global/extensions.js
+++ src/global/extensions.js
@@ -1,3 +1,7 @@
 Array.prototype.isEqual = function(array) {
     return this.length === array.length && this.every((el, i) => el === array[i]);
 };
+
+Date.intervalSince = function(milliseconds) {
+    return Date.now() - milliseconds;
+};
--- src/index.js
+++ src/index.js
@@ -4,14 +4,23 @@ import "p5";
 import createSketch from "./components/createSketch";
 import Color from "./components/Color";
 import * as mouseMemory from "./components/mouseMemory";
+import './global/extensions';
 
 const backgroundColor = new Color(0, 100, 200);
+const startTime = Date.now();
 createSketch(backgroundColor);
 
 window.mouseClicked = () => {
     mouseMemory.handleClick(window.mouseX, window.mouseY, backgroundColor);
 };
 
+window.keyPressed = () => {
+  console.log(`key: ${window.key}`);
+};
+
 window.draw = () => {
     mouseMemory.drawCurrentMode(backgroundColor);
+    window.textSize(20);
+    window.text(`${Math.floor((Date.now() - startTime) / 1000)}`, 200, 200);
+    window.text(`${Math.floor(Date.intervalSince(startTime) / 1000)}`, 200, 230);
 };
