b797e18acb7a519711485c699479a823dc7595c6
cszatma <christopherszatmary@gmail.com>
2017-12-14 14:17:59 -0500

    Added ability to pass a function to createSketch to perform any additional setup

--- src/components/createSketch.js
+++ src/components/createSketch.js
@@ -4,7 +4,7 @@ import "p5/lib/addons/p5.dom";
 import Color from "./Color";
 import { canvasSize } from "../global/constants";
 
-export default function createSketch(color: Color = new Color(255)) {
+export default function createSketch(color: Color = new Color(255), additionalSetup?: void => void) {
     window.setup = () => {
         window.createCanvas(canvasSize.width, canvasSize.height);
         window.background(color.p5Color);
@@ -12,5 +12,6 @@ export default function createSketch(color: Color = new Color(255)) {
             window.clear();
             window.background(color.p5Color);
         });
+        typeof additionalSetup === "function" && additionalSetup();
     }
 }
