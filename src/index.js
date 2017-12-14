b4d74582ecb998825eb595729e51629ee4ba89a6
cszatma <christopherszatmary@gmail.com>
2017-12-14 11:58:44 -0500

    Imported styles from index.css

--- src/index.js
+++ src/index.js
@@ -4,7 +4,8 @@ import "p5";
 import createSketch from "./components/createSketch";
 import Color from "./components/Color";
 import * as mouseMemory from "./components/mouseMemory";
-import './global/extensions';
+import "./global/extensions";
+import "./styles/index.css";
 
 const backgroundColor = new Color(0, 100, 200);
 const startTime = Date.now();
