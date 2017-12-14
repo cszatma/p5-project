24f2e2465273638c360ce56e0f52ece1cdc61e83
cszatma <christopherszatmary@gmail.com>
2017-12-14 14:19:37 -0500

    Added new sketches

---
+++ src/sketches/functionMonster.js
@@ -0,0 +1,27 @@
+import { w } from "../global/constants";
+
+export default function drawFunctionMonster() {
+    function drawBody() {
+        w.fill(152, 234, 175);
+        w.ellipse(200, 200, 300, 300);
+    }
+
+    function drawMouth() {
+        w.fill(255, 0, 0);
+        w.arc(200, 250, 100, 100, 0, Math.PI);
+    }
+
+    function drawEye(x: number, y: number) {
+        w.fill(255);
+        w.ellipse(x, y, 80, 80);
+        w.fill(0);
+        w.ellipse(x, y, 20, 20);
+    }
+
+    (() => {
+        drawBody();
+        drawMouth();
+        drawEye(150, 50);
+        drawEye(250, 50);
+    })();
+}
---
+++ src/sketches/squareRandomDragger.js
@@ -0,0 +1,42 @@
+// @flow
+
+import { canvasSize, w } from "../global/constants";
+import Color from "../components/Color";
+
+const halfWidth = canvasSize.width / 2;
+const halfHeight = canvasSize.height / 2;
+let lastSquare = 0;
+let currentSquare = 0;
+
+function setupScene() {
+    w.strokeWeight(5);
+    w.line(halfWidth, 0, halfWidth, canvasSize.height);
+    w.line(0, halfHeight, canvasSize.width, halfHeight);
+}
+
+function changeColor() {
+    if (lastSquare !== currentSquare) {
+        w.fill(Color.randomP5Color());
+        lastSquare = currentSquare;
+    }
+}
+
+function drawSquares() {
+    const x = w.mouseX < halfWidth ? 0 : halfWidth;
+    const y = w.mouseY < halfHeight ? 0 : halfHeight;
+
+    if (w.mouseX < halfWidth) {
+        currentSquare = y < halfHeight ? 0 : 1;
+    } else {
+        currentSquare = y < halfHeight ? 2 : 3;
+    }
+
+    w.rect(x, y, halfWidth, halfHeight);
+}
+
+function draw() {
+    changeColor();
+    drawSquares();
+}
+
+export { setupScene, draw };
