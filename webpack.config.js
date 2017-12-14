ae5621c4964e855abdfbc03ea2c95e08f1c92288
cszatma <christopherszatmary@gmail.com>
2017-12-14 11:56:47 -0500

    Changed babel-loader to use flow preset, added ExtractTextPlugin to generate css files on build, added HtmlWebpackPlugin to generate an index.html file

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,16 @@
-const path = require('path');
-const webpack = require('webpack');
+const path = require("path");
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
-const resolve = path.resolve,
-    join = path.join;
+const resolve = path.resolve, join = path.join;
 
 const rootDir = resolve(__dirname);
-const srcDir = join(rootDir, 'src');
-const buildDir = join(rootDir, 'build');
+const srcDir = join(rootDir, "src");
+const buildDir = join(rootDir, "build");
 
 module.exports = {
-    entry: join(srcDir, 'index.js'),
+    entry: join(srcDir, "index.js"),
     module: {
         rules: [
             {
@@ -17,19 +18,59 @@ module.exports = {
                 include: srcDir,
                 exclude: /(node_modules|bower_components)/,
                 use: {
-                    loader: 'babel-loader',
+                    loader: "babel-loader",
                     options: {
-                        presets: ['flow']
+                        presets: ["flow"]
                     }
                 }
+            },
+            {
+                test: /\.css$/,
+                include: srcDir,
+                use: ExtractTextPlugin.extract({
+                    fallback: {
+                        loader: require.resolve('style-loader'),
+                        options: {
+                            hmr: false,
+                        },
+                    },
+                    use: [
+                        {
+                            loader: require.resolve('css-loader'),
+                            options: {
+                                importLoaders: 1,
+                                minimize: true,
+                            }
+                        }
+                    ]
+                })
             }
         ]
     },
     output: {
         path: buildDir,
-        filename: 'index.bundle.js'
+        filename: "index.bundle.js"
     },
     devServer: {
         contentBase: buildDir
-    }
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: "p5 Example",
+            inject: true,
+            minify: {
+                removeComments: true,
+                collapseWhitespace: true,
+                removeRedundantAttributes: true,
+                useShortDoctype: true,
+                removeEmptyAttributes: true,
+                removeStyleLinkTypeAttributes: true,
+                keepClosingSlash: true,
+                minifyJS: true,
+                minifyCSS: true,
+                minifyURLs: true,
+            }
+        }),
+        new ExtractTextPlugin("index.css")
+    ]
 };
