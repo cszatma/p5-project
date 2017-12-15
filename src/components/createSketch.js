e0b515f420e1b716e9a50a3e43f6d32bd6cad5bf
cszatma <christopherszatmary@gmail.com>
2017-12-14 23:19:29 -0500

    Added resetHandler parameter to createSketch() to allow extra code to be called when the reset button is clicked

--- src/components/createSketch.js
+++ src/components/createSketch.js
@@ -4,13 +4,14 @@ import "p5/lib/addons/p5.dom";
 import Color from "../graphics/Color";
 import { canvasSize } from "../global/constants";
 
-export default function createSketch(color: Color = new Color(255), additionalSetup?: void => void) {
+export default function createSketch(color: Color = new Color(255), additionalSetup?: void => void, resetHandler?: void => void) {
     window.setup = () => {
         window.createCanvas(canvasSize.width, canvasSize.height);
         window.background(color.p5Color);
         window.createButton("Reset").mousePressed(() => {
             window.clear();
             window.background(color.p5Color);
+            typeof resetHandler === "function" && resetHandler();
         });
         typeof additionalSetup === "function" && additionalSetup();
     }
