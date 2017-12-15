25393c2b3778e2013eab151504de4350fe8182c5
cszatma <christopherszatmary@gmail.com>
2017-12-14 19:58:06 -0500

    Added methods to Point and Size allowing them to be converted to and from tuples, added methods for testing equality

--- src/graphics/Point.js
+++ src/graphics/Point.js
@@ -8,6 +8,27 @@ class Point {
         this.x = x;
         this.y = y;
     }
+
+    get tupleValue(): [number, number] {
+        return [this.x, this.y];
+    }
+
+    set tupleValue(newValue: [number, number]) {
+        this.x = newValue[0];
+        this.y = newValue[1];
+    }
+
+    isEqual(point: Point): boolean {
+        return this.x === point.x && this.y === point.y;
+    }
+
+    static fromTuple(tuple: [number, number]): Point {
+        return new Point(tuple[0], tuple[1]);
+    }
+
+    static get zero(): Point {
+        return new Point(0, 0);
+    }
 }
 
 export default Point;
--- src/graphics/Size.js
+++ src/graphics/Size.js
@@ -8,6 +8,32 @@ class Size {
         this.width = width;
         this.height = height;
     }
+
+    get tupleValue(): [number, number] {
+        return [this.width, this.height];
+    }
+
+    set tupleValue(newValue: [number, number]) {
+        this.width = newValue[0];
+        this.height = newValue[1];
+    }
+
+    incrementBy(amount: number) {
+        this.width += amount;
+        this.height += amount;
+    }
+
+    isEqual(size: Size): boolean {
+        return this.width === size.width && this.height === size.width;
+    }
+
+    static withSide(side: number): Size {
+        return new Size(side, side);
+    }
+
+    static fromTuple(tuple: [number, number]): Size {
+        return new Size(tuple[0], tuple[1]);
+    }
 }
 
 export default Size;
