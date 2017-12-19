27a5cccaf1b4877c8ee2b02b56120afdebbb6340
Christopher Szatmary <christopherszatmary@gmail.com>
2017-12-18 22:20:57 -0500

    Replaced ColorPicker.colors with ColorPicker._colorsRect and array of Rectangles

--- src/components/ColorPicker.js
+++ src/components/ColorPicker.js
@@ -1,31 +1,35 @@
 // @flow
 
-import { Color, Point, Size } from  "../graphics/Graphics";
+import { Color, Point, Rectangle, Size } from  "../graphics/Graphics";
 import { Direction } from "../global/constants";
 import { directionMatrix } from "../global/functions";
 
 class ColorPicker {
-    colors: Color[];
-    origin: Point;
-    size: Size;
-    direction: Direction;
+    _colorRects: Rectangle[];
+    _origin: Point;
+    _size: Size;
+    _direction: Direction;
 
     constructor(colors: Color[], origin: Point, size: Size, direction: Direction) {
-        this.colors = colors;
-        this.origin = origin;
-        this.size = size;
-        this.direction = direction;
+        this._colorRects = [];
+        this._origin = origin;
+        this._size = size;
+        this._direction = direction;
+        this._generateRects(colors);
+    }
+
+    _generateRects(colors: Color[]) {
+        let x = this._origin.x, y = this._origin.y;
+        let [horizontal, vertical] = directionMatrix(this._direction);
+        for (let i = 0; i < colors.length; i++) {
+            this._colorRects.push(new Rectangle(new Point(x, y), this._size, colors[i]));
+            x += this._size.width * horizontal;
+            y += this._size.height * vertical;
+        }
     }
 
     draw() {
-        let x = this.origin.x, y = this.origin.y;
-        let [horizontal, vertical] = directionMatrix(this.direction);
-        for (let i = 0; i < this.colors.length; i++) {
-            window.fill(this.colors[i].p5Color);
-            window.rect(x, y, this.size.width, this.size.height);
-            x += this.size.width * horizontal;
-            y += this.size.height * vertical;
-        }
+        this._colorRects.forEach(rect => rect.draw());
     }
 }
 
