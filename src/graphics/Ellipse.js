2cca321b06f4479f8e9a86aafbba830577a5c609
Christopher Szatmary <christopherszatmary@gmail.com>
2017-12-22 01:07:28 -0500

    Added Ellipse class, added optional color parameter to Rectangle.p5Rect()

--- src/graphics/Ellipse.js
+++ src/graphics/Ellipse.js
@@ -4,7 +4,7 @@ import Color from "./Color";
 import Point from "./Point";
 import Size from "./Size";
 
-export default class Rectangle {
+export default class Ellipse {
     origin: Point;
     size: Size;
     color: Color;
@@ -17,15 +17,15 @@ export default class Rectangle {
 
     draw() {
         window.fill(this.color.p5Color);
-        window.rect(this.origin.x, this.origin.y, this.size.width, this.size.height)
+        window.ellipse(this.origin.x, this.origin.y, this.size.width, this.size.height);
     }
 
-    static init(x: number, y: number, width: number, height: number, color: Color): Rectangle {
-        return new Rectangle(new Point(x, y), new Size(width, height), color);
+    static init(x: number, y: number, width: number, height: number, color: Color): Ellipse {
+        return new Ellipse(new Point(x, y), new Size(width, height), color);
     }
 
     static p5Rect(origin: Point, size: Size, color?: Color) {
         color !== undefined && window.fill(color.p5Color);
-        window.rect(origin.x, origin.y, size.width, size.height);
+        window.ellipse(origin.x, origin.y, size.width, size.height);
     }
 }
--- src/graphics/Graphics.js
+++ src/graphics/Graphics.js
@@ -1,7 +1,8 @@
 import Color from "./Color";
+import Ellipse from "./Ellipse";
 import Point from "./Point";
 import Rectangle from "./Rectangle";
 import Size from "./Size";
 import Text from "./Text";
 
-export { Color, Point, Rectangle, Size, Text };
+export { Color, Ellipse, Point, Rectangle, Size, Text };
