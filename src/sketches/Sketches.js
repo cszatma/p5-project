b3b68df4912270d36ab15ceb2df08724dc8763ae
cszatma <christopherszatmary@gmail.com>
2017-12-30 14:28:02 -0500

    Removed Sketch export from Sketches.js

--- src/sketches/Sketches.js
+++ src/sketches/Sketches.js
@@ -1,9 +1,7 @@
-import Sketch from './Sketch';
-
 import mouseMemory from "./MouseMemory";
 import * as squareRandomDragger from "./squareRandomDragger";
 import functionMonster from "./FunctionMonster";
 import apocalypse from "./Apocalypse";
 import * as Snake from "./Snake";
 
-export { mouseMemory, squareRandomDragger, functionMonster, apocalypse, Sketch, Snake };
+export { mouseMemory, squareRandomDragger, functionMonster, apocalypse, Snake };
