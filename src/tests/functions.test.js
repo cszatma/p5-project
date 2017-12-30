5008993546f9c31af554eff9f4dc9bd96fe0ca79
cszatma <christopherszatmary@gmail.com>
2017-12-29 21:00:50 -0500

    Added jest.config and setup modulePaths variable to allow jest to use absolute imports, placed all function tests into a describe block"

--- src/tests/functions.test.js
+++ src/tests/functions.test.js
@@ -1,18 +1,20 @@
-import { addBinaryTuples, directionMatrix, isOutOfBounds, scaleBinaryTuple } from "../global/functions";
-import { Direction } from "../global/constants";
-import Point from "../graphics/Point";
+import * as fn from "global/functions";
+import { Direction } from "global/constants";
+import Point from "graphics/Point";
 
-test("(550, 78) should be out of bounds", () =>
-    expect(isOutOfBounds(new Point(550, 78))).toBe(true));
+describe("Test all global functions", () => {
+    test("(550, 78) should be out of bounds", () =>
+        expect(fn.isOutOfBounds(new Point(550, 78))).toBe(true));
 
-test("(56, 148) should be within bounds", () =>
-    expect(isOutOfBounds(new Point(56, 148))).toBe(false));
+    test("(56, 148) should be within bounds", () =>
+        expect(fn.isOutOfBounds(new Point(56, 148))).toBe(false));
 
-test("Direction.left tuple representation should be [-1, 0]", () =>
-    expect(directionMatrix(Direction.left)).toEqual([-1, 0]));
+    test("Direction.left tuple representation should be [-1, 0]", () =>
+        expect(fn.directionMatrix(Direction.left)).toEqual([-1, 0]));
 
-test("adding tuples [2, 5], [3, 8] should return [5, 13]", () =>
-    expect(addBinaryTuples([2, 5], [3, 8])).toEqual([5, 13]));
+    test("adding tuples [2, 5], [3, 8] should return [5, 13]", () =>
+        expect(fn.addBinaryTuples([2, 5], [3, 8])).toEqual([5, 13]));
 
-test("scaling [3, 5] by 3 should return [9, 15]", () =>
-    expect(scaleBinaryTuple([3, 5], 3)).toEqual([9, 15]));
+    test("scaling [3, 5] by 3 should return [9, 15]", () =>
+        expect(fn.scaleBinaryTuple([3, 5], 3)).toEqual([9, 15]));
+});
